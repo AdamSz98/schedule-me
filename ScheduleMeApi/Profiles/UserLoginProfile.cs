f1d4a0f194d3967ee20237d9c56c614f6afad994
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-06-19 20:52:15 +0200

    Create UserLoginDto

---
+++ ScheduleMeApi/Profiles/UserLoginProfile.cs
@@ -0,0 +1,22 @@
+﻿namespace ScheduleMeApi.Profiles
+{
+    public class UserLoginProfile : Profile
+    {
+        public UserLoginProfile()
+        {
+            CreateMap<UserLoginDto, User>()
+                .ForMember(
+                    dest => dest.Username,
+                    opt => opt.MapFrom(src => src.Username)
+                )
+                .ForMember(
+                    dest => dest.Email,
+                    opt => opt.MapFrom(src => src.Email)
+                )
+                .ForMember(
+                    dest => dest.Password,
+                    opt => opt.MapFrom(src => src.Password)
+                );
+        }
+    }
+}
