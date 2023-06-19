55d3c23221e2a2ba9d9eb9839250f7be74bc3b14
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-06-19 20:07:59 +0200

    Add DbContext file and automapper for registration

---
+++ ScheduleMeApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace ScheduleMeApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- ScheduleMeApi/GlobalUsings.cs
+++ ScheduleMeApi/GlobalUsings.cs
@@ -1,3 +1,9 @@
 ﻿global using System.ComponentModel.DataAnnotations;
+global using System.Text.Json.Serialization;
+
+global using Microsoft.EntityFrameworkCore;
+global using AutoMapper;
 
 global using ScheduleMeApi.Models;
+global using ScheduleMeApi.Data;
+global using ScheduleMeApi.DTO.Incoming;
