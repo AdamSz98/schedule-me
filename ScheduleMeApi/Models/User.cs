9659c79439bb147ef7ad641fa0ce5bf15059faf8
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-06-19 19:38:34 +0200

    Create user model

---
+++ ScheduleMeApi/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace ScheduleMeApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; } = string.Empty;
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
