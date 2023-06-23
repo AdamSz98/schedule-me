c3a16b439b5379b3fe8b86a13ad3736a363ab8a2
PatakiGergo <pataki821@gmail.com>
2023-06-23 13:12:55 +0200

    Register page
    Styled register page

--- schedule-me-app/app/components/userforms/RegisterForm.tsx
+++ schedule-me-app/app/components/userforms/RegisterForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import * as React from "react";
 import styles from "./styles/registerForm.module.css";
 import Link from "next/link";
@@ -77,60 +78,110 @@ export default function RegisterForm(props: IRegisterFormProps) {
   }
 
   return (
-    <div className={styles.registerForm}>
-      <div className={styles.intro}>
-        <p className={styles.welcomeText}>Sign up now 👋</p>
-        <p className={styles.welcomeAltText}>
-          Today is a new day. It's your day. You shape it. Sign in to start
-          managing your projects.
-        </p>
-      </div>
-
-      <form action="" onSubmit={handleSubmit}>
-        <div className={styles.name}>
-          <input
-            type="text"
-            placeholder="First name"
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-          <input
-            type="text"
-            placeholder="Last name"
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-          <input
-            type="text"
-            placeholder="Username"
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    <>
+      <div className={styles.registerForm}>
+        <div className={styles.intro}>
+          <p className={styles.welcomeText}>Join us today👋</p>
         </div>
-        <input
-          type="email"
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-          size={30}
-          required
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="password again"
-          onChange={(e) => setPasswordAgain(e.target.value)}
-          required
-        />
-        <button className={styles.registerButton}>Register</button>
-        <p>
-          Already have an account? <Link href="/login">Login here.</Link>
-        </p>
-      </form>
-    </div>
+
+        <form action="" onSubmit={handleSubmit}>
+          <div className={styles.inputContainer}>
+            <label htmlFor="firstname">First name:</label>
+            <input
+              id="firstname"
+              type="text"
+              placeholder="First name"
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+            <label htmlFor="lastname">Last name:</label>
+            <input
+              id="lastname"
+              type="text"
+              placeholder="Last name"
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <label htmlFor="username">Username:</label>
+            <input
+              id="username"
+              type="text"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="email">Email:</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="example@email.com"
+              onChange={(e) => setEmail(e.target.value)}
+              size={30}
+              required
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />{" "}
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="passwordagain">Password again:</label>
+            <input
+              id="passwordagain"
+              type="password"
+              placeholder="password again"
+              onChange={(e) => setPasswordAgain(e.target.value)}
+              required
+            />
+          </div>
+
+          <button className={styles.loginButton}>
+            <span>Register</span>
+          </button>
+        </form>
+        <div className={styles.socialLogin}>
+          <div className={styles.break}>
+            <div className={styles.breakLine}></div>
+            <p>OR</p>
+            <div className={styles.breakLine}></div>
+          </div>
+          <div className={styles.socialButtons}>
+            <button type="button" className={styles.socialButton}>
+              <Image
+                src="/google_icon.png"
+                width={28}
+                height={28}
+                alt="google icon"
+              ></Image>
+              <p>Sign in with Google</p>
+            </button>
+            <button type="button" className={styles.socialButton}>
+              <Image
+                src="/facebook_icon.png"
+                width={28}
+                height={28}
+                alt="google icon"
+              ></Image>
+              <p>Sign in with Facebook</p>
+            </button>
+          </div>
+          <p className={styles.noAccount}>
+            Already have an account?
+            <Link href="/login">Sign in</Link>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
